e44fa33ae73fe03b3649b21f83339da2e20dc2d8
gitlood <unilood@gmail.com>
2023-01-14 16:46:24 +1300

    Error Indicator added

--- app/src/main/java/com/example/android/amphibiansapp/ui/HomeScreenDisplay.kt
+++ app/src/main/java/com/example/android/amphibiansapp/ui/HomeScreenDisplay.kt
@@ -1,12 +1,17 @@
 package com.example.android.amphibiansapp.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Block
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.android.amphibiansapp.ui.mainscreen.AmphibianViewModel
 import com.example.android.amphibiansapp.ui.mainscreen.ResourceState
@@ -20,15 +25,10 @@ fun HomeScreen(viewModel: AmphibianViewModel = hiltViewModel()) {
     Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         when (uiState.value.state) {
             ResourceState.LOADING -> LoadingIndicator()
-            ResourceState.SUCCESS -> Amphibians()
+            ResourceState.SUCCESS -> ErrorIndicator()
             ResourceState.ERROR -> ErrorIndicator()
         }
     }
-}
-
-@Composable
-fun LoadingIndicator() {
-    CircularProgressIndicator()
 }
 
 @Composable
@@ -37,8 +37,17 @@ fun Amphibians() {
 }
 
 @Composable
+fun LoadingIndicator() {
+    CircularProgressIndicator()
+}
+
+@Composable
 fun ErrorIndicator() {
-    //CircularProgressIndicator(modifier = Modifier.fillMaxSize())
+    Image(
+        imageVector = Icons.Rounded.Block,
+        contentDescription = "Error",
+        modifier = Modifier.size(100.dp)
+    )
 }
 
 
