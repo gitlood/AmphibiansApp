63292f4fd33440b38f3a4249dc0c90d05d5ffc4b
gitlood <unilood@gmail.com>
2023-01-14 14:47:49 +1300

    Gradle update for dagger hilt

---
+++ app/src/main/java/com/example/android/amphibiansapp/AmphibiansApp.kt
@@ -0,0 +1,7 @@
+package com.example.android.amphibiansapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AmphibiansApp:Application()
---
+++ app/src/main/java/com/example/android/amphibiansapp/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.example.android.amphibiansapp.di
+
+class AppModule {
+}
